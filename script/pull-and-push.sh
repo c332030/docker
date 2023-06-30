789cb9f5f8ca51ce7fb935ecfaa7e6b37ca339b4
c332030 <c332030@c332030.com>
2023-06-30 14:37:25 +0800

    feat: clean image

--- script/pull-and-push.sh
+++ script/pull-and-push.sh
@@ -15,4 +15,7 @@ docker pull ${ORIGIN_IMAGE}
 docker login -u ${USERNAME} -p ${PASSWORD} ${REGISTRY}
 docker image tag ${ORIGIN_IMAGE} ${REGISTRY}/${IMAGE_NAME}
 docker push ${REGISTRY}/${IMAGE_NAME}
+
 docker logout ${REGISTRY}
+
+docker system prune -af

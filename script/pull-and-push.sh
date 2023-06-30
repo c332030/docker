2b4e6dbdb7a1ca71276cb7e4cab78c6dae0ddc21
c332030 <c332030@c332030.com>
2023-06-30 13:47:14 +0800

    feat: print

--- script/pull-and-push.sh
+++ script/pull-and-push.sh
@@ -4,6 +4,11 @@ echo 'pull-and-push'
 
 set -e
 
+echo "
+ORIGIN_IMAGE: ${ORIGIN_IMAGE}
+REGISTRY: ${REGISTRY}
+"
+
 docker pull ${ORIGIN_IMAGE}
 
 docker login -u ${USERNAME} -p ${PASSWORD} ${REGISTRY}
